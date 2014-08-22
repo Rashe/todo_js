221dc381ed7ba9e58704e6e33cbc55aad1ada5fe
rashe <shemeshra@gmail.com>
2014-08-22 16:42:44 +0300

    base validation

--- routes/login.js
+++ routes/login.js
@@ -8,20 +8,28 @@ exports.post = function (req, res, next) {
     var qRes = res,
         user = req.body.username,
         pass = req.body.password;
-    User.findOne({username: user}, function (err, userDb, next) {
-        if (userDb == null) {
-            res.writeHead(403, {"Content-Type": "text/plain"});
-            //res.end('"Такова пацанчика нет"');
-            res.end(errors.no_such_user);
-        } else if (userDb.password != pass) {
-            res.writeHead(403, {"Content-Type": "text/plain"});
-            res.end(errors.pass_wrong);
-        }
-        else {
-            req.session.user = user;
-            qRes.send({});
-        }
 
-    });
+    if (user == '' || user == null) {
+        res.writeHead(403, {"Content-Type": "text/plain"});
+        res.end(errors.fuck_you);
+    } else if (pass == '' || pass == null) {
+        res.writeHead(403, {"Content-Type": "text/plain"});
+        res.end(errors.fuck_you);
+    } else {
+        User.findOne({username: user}, function (err, userDb, next) {
+            if (userDb == null) {
+                res.writeHead(403, {"Content-Type": "text/plain"});
+                //res.end('"Такова пацанчика нет"');
+                res.end(errors.no_such_user);
+            } else if (userDb.password != pass) {
+                res.writeHead(403, {"Content-Type": "text/plain"});
+                res.end(errors.pass_wrong);
+            }
+            else {
+                req.session.user = user;
+                qRes.send({});
+            }
 
+        });
+    }
 };
--- routes/regi.js
+++ routes/regi.js
@@ -9,25 +9,31 @@ exports.post = function (req, res, next) {
         user = req.body.username,
         pass = req.body.password;
 
-    User.findOne({username: user}, function (err, userDb, next) {
-        if (userDb != null) {
-            res.writeHead(403, {"Content-Type": "text/plain"});
-            res.end(errors.already_registered);
-        }
-        else {
+    if (user == '' || user == null) {
+        res.writeHead(403, {"Content-Type": "text/plain"});
+        res.end(errors.fuck_you);
+    } else if (pass == '' || pass == null) {
+        res.writeHead(403, {"Content-Type": "text/plain"});
+        res.end(errors.fuck_you);
+    } else {
+        User.findOne({username: user}, function (err, userDb, next) {
+            if (userDb != null) {
+                res.writeHead(403, {"Content-Type": "text/plain"});
+                res.end(errors.already_registered);
+            }
+            else {
 
-            var createUser = new User({
-                username: user,
-                password: pass
-            });
+                var createUser = new User({
+                    username: user,
+                    password: pass
+                });
 
-            createUser.save(function (err) {
-                if (err) throw  err;
-            });
-            req.session.user = user;
-            qRes.send({});
-        }
-    });
-
-
+                createUser.save(function (err) {
+                    if (err) throw  err;
+                });
+                req.session.user = user;
+                qRes.send({});
+            }
+        });
+    }
 };
